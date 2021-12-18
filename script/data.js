65ea43c63c9e5b4ce632df4934788252987d33a1
himchan <himchan9494@gmail.com>
2021-12-19 03:45:40 +0900

    feat: Fetching 2 account data
    Pushed data to  variable "accountArray"

---
+++ script/data.js
@@ -0,0 +1,23 @@
+let accountArray = [];
+
+const myAccountUrl =
+  "https://gist.githubusercontent.com/himchan94/a539fd8c884477a314044e8b423b9653/raw/d721b447772084c4e51c26daaf73b9c75c410780/myAccount.json";
+const gmAccountUrl =
+  "https://gist.githubusercontent.com/himchan94/283d5837431bec8d5cb88a6e3525c35f/raw/0db5aa5a950082ea84a13b2146e40ef0ec3c8eaf/grandmotherAccount.json";
+
+const getData = async () => {
+  await fetch(myAccountUrl)
+    .then((res) => res.json())
+    .then((data) => accountArray.push(data));
+
+  await fetch(gmAccountUrl)
+    .then((res) => res.json())
+    .then((data) => accountArray.push(data))
+    .then(() => {
+      const script = document.createElement("script");
+      script.src = "./script/main.js";
+      document.getElementsByTagName("head")[0].appendChild(script);
+    });
+};
+
+getData();
