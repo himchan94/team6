f42704fa7deab0bc19d979faeb26acd721d4019e
himchan <himchan9494@gmail.com>
2021-12-18 16:03:21 +0900

    feat: Dragbar event done
    Added touchEvent to dragbar

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
-const getData = () => {
-  const data = fetch(
-    "https://gist.githubusercontent.com/himchan94/a539fd8c884477a314044e8b423b9653/raw/9ef92298659dee864e899bd14babf83cc8f603da/myAccount.json"
-  )
-    .then((res) => res.json())
-    .then((data) => console.log(data));
-};
+const spendSection = document.querySelector(".spend-section");
+const dragBar = spendSection.querySelector(".spend-section > .dragbar");
+const limit = document.querySelector(".limit");
+const accountMoney = document.querySelector(".account-money");
 
-getData();
+const accountMoneyBottom = accountMoney.getBoundingClientRect().bottom;
+const limitTop = limit.getBoundingClientRect().top;
+
+let middleLine = (limitTop + accountMoneyBottom) / 2;
+// middleLine(현재 161.5)를 기준
+
+let original = spendSection.clientHeight;
+// spend-section의 기본 높이 373px
+
+dragBar.addEventListener("touchmove", (e) => {
+  let newHeight = 680 - e.touches[0].clientY;
+  spendSection.style.height = `${newHeight}px`;
+});
+
+dragBar.addEventListener("touchend", (e) => {
+  let endPoint = e.changedTouches[0].clientY;
+  // 터치 이벤트 종료시점의 clinetY 기준으로 최소, 최대 높이를 결정
+  if (endPoint < middleLine) {
+    spendSection.style.height = "637px";
+    // 최대 높이
+  } else {
+    spendSection.style.height = "387px";
+    // 최소 높이
+  }
+});
