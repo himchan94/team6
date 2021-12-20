d7e1e1f6d2c911d336de21614cedc52613e57ca3
himchan <himchan9494@gmail.com>
2021-12-20 15:06:49 +0900

    feat: Removed unnecessary code

--- script/getData.js
+++ script/getData.js
@@ -3,7 +3,7 @@ let accountArray = [];
 const myAccountUrl =
   "https://gist.githubusercontent.com/himchan94/a539fd8c884477a314044e8b423b9653/raw/d721b447772084c4e51c26daaf73b9c75c410780/myAccount.json";
 const gmAccountUrl =
-  "https://gist.githubusercontent.com/himchan94/283d5837431bec8d5cb88a6e3525c35f/raw/0db5aa5a950082ea84a13b2146e40ef0ec3c8eaf/grandmotherAccount.json";
+  "https://gist.githubusercontent.com/himchan94/283d5837431bec8d5cb88a6e3525c35f/raw/dfb9f2f8283d6ca89411f9a3b4b448aaeef89ab2/grandmotherAccount.json";
 
 const getData = async () => {
   await fetch(myAccountUrl)
@@ -22,3 +22,7 @@ const getData = async () => {
 };
 
 getData();
+
+function sayHello() {
+  console.log("hi");
+}
